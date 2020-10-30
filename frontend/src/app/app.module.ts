d79bacefe90a45b0e47820d191988da8e3f37956
EN <elaynenatalia@consiste.com.br>
2020-10-30 08:25:14 -0300

    Update

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,83 @@
+import { MatTableModule } from '@angular/material/table';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { HomeComponent } from './views/home/home.component';
+
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule} from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+import { FormsModule } from '@angular/forms';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    ProductCreateComponent,
+    RedDirective,
+    ForDirective,
+    ProductReadComponent,
+    ProductUpdateComponent
+  ],
+  imports: [
+    MatSortModule,
+    MatTableModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    MatPaginatorModule
+  ],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+  MatTableModule
+}
+
---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+
+  constructor() { 
+
+  }
+
+  ngOnInit(){
+
+  }
+
+
+}
